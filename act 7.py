a24b6597fc932af22c2ae420f75331cf923b1bb2
Juanpiccot12 <Jd5910445@gmail.com>
2025-06-06 11:36:06 -0300

    Add files via upload

---
+++ act 7.py
@@ -0,0 +1,12 @@
+vocales = [ 'a,e,i,o,u' ]
+palabras = ""
+
+palabras = input("Ingrese una letra")
+ 
+while palabras == vocales:
+    break
+else:
+    palabras = input("Ingrese una letra")
+    
+print(palabras)
+    
